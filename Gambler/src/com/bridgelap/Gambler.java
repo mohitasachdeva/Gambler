2786efbfd8f34ec9898c21c37e3fbb1870b0989c
HP <sachdevamohita5@gmail.com>
2022-07-16 14:25:50 +0530

    uc1

---
+++ Gambler/src/com/bridgelap/Gambler.java
@@ -0,0 +1,14 @@
+package com.bridgelap;
+import java.util.*;
+public class Gambler {
+    static final int INITIAL_STAKE=100;
+    static final int BET_AMOUNT=1;
+
+    public static void main(String[] args) {
+        System.out.println("Initial Amount = $"+INITIAL_STAKE);
+        System.out.println("Bet Amount = $"+BET_AMOUNT);
+    }
+}
+
+
+
